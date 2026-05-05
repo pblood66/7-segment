ce8a68d177befd89eff65f61a1c6c2130fabba31
Patrick Blood <pblood66@gmail.com>
2026-05-05 15:14:53 -0600

    add horizontal and vertical segment

--- src/main.c
+++ src/main.c
@@ -7,21 +7,37 @@
 #define SEGMENT_WIDTH 200
 #define SEGMENT_HEIGHT 50
 
-void DrawSegment(Vector2 center)
+void DrawSegment(Vector2 center, bool horizontal)
 {
     int count = 6;
+    int width = SEGMENT_WIDTH / 2;
+    int height = SEGMENT_HEIGHT / 2;
 
     Vector2 a, b, c, d, e, f;
-    a = (Vector2){center.x - SEGMENT_WIDTH / 2 - SEGMENT_HEIGHT / 2, center.y};
-    b = (Vector2){center.x - SEGMENT_WIDTH / 2, center.y + SEGMENT_HEIGHT / 2};
-    c = (Vector2){center.x - SEGMENT_WIDTH / 2, center.y - SEGMENT_HEIGHT / 2};
-    d = (Vector2){center.x + SEGMENT_WIDTH / 2, center.y + SEGMENT_HEIGHT / 2};
-    e = (Vector2){center.x + SEGMENT_WIDTH / 2, center.y - SEGMENT_HEIGHT / 2};
-    f = (Vector2){center.x + SEGMENT_WIDTH / 2 + SEGMENT_HEIGHT / 2, center.y};
+
+    if (horizontal)
+    {
+        a = (Vector2){center.x - width - height, center.y};
+        b = (Vector2){center.x - width, center.y + height};
+        c = (Vector2){center.x - width, center.y - height};
+        d = (Vector2){center.x + width, center.y + height};
+        e = (Vector2){center.x + width, center.y - height};
+        f = (Vector2){center.x + width + height, center.y};
+    }
+    else
+    {
+        a = (Vector2){center.x, center.y + width + height};
+        b = (Vector2){center.x + height, center.y + width};
+        c = (Vector2){center.x - height, center.y + width};
+        d = (Vector2){center.x + height, center.y - width};
+        e = (Vector2){center.x - height, center.y - width};
+        f = (Vector2){center.x, center.y - width - height};
+    }
     Vector2 points[] = {a, b, c, d, e, f};
 
-
+    BeginDrawing();
     DrawTriangleStrip(points, 6, RED);
+    EndDrawing();
 }
 
 int main(int argc, char *argv[])
@@ -37,10 +53,11 @@ int main(int argc, char *argv[])
     while (!WindowShouldClose())
     {
         BeginDrawing();
-    
+
         Vector2 center = {WIDTH / 2, HEIGHT / 2};
-        DrawSegment(center);
-       
+        DrawSegment(center, false);
+        DrawSegment(center, true);
+
         EndDrawing();
     }
 
